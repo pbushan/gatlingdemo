3fe04a6b7684920a95e9ba8a188d719858cb83de
Phani Bushan <pbushan@yahoo.com>
2020-02-13 19:15:11 -0500

    Initial commit

---
+++ src/test/java/com/pbstech/gatling/GatlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbstech.gatling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
