d7460a64875f9b2720f99e7c22f0376bb7755d37
Phani Bushan <pbushan@yahoo.com>
2020-02-14 19:34:04 -0500

    Added another step to get variable response times

--- src/test/java/com/pbstech/gatling/Steps.scala
+++ src/test/java/com/pbstech/gatling/Steps.scala
@@ -1,12 +1,20 @@
 package com.pbstech.gatling
 
 import io.gatling.core.Predef._
+import io.gatling.core.feeder._
 import io.gatling.http.Predef._
 
 object Product {
 
+  def orderRef() = new scala.util.Random().nextInt(1500)
+
   val getProduct = exec(http("/Product")
     .get("/product")
     .check(jsonPath("$.id").exists.saveAs("productId")))
+
+  val getdelayed = exec(http("/getdelayed")
+    .get("/getdelayed/" + orderRef())
+    .check(jsonPath("$.id").exists.saveAs("productId")))
+
 
 }
